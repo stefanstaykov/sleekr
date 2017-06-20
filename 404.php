76254c571880cfb4dfa06f3dc89f1ab785c86cca
whf-chefo <chefopld@gmail.com>
2017-06-20 16:46:58 +0300

    Update 404.php

--- 404.php
+++ 404.php
@@ -1,4 +1,13 @@
-<?php get_header(); ?>
+<?php
+/**
+ * The template for displaying 404 pages (not found)
+ *
+ * @link https://codex.wordpress.org/Creating_an_Error_404_Page
+ *
+ * @package Sleekr Lite
+ */
+
+get_header(); ?>
 
     <!-- Page Content -->
     <div class="container">
@@ -6,40 +15,39 @@
         <!-- Page Heading/Breadcrumbs -->
         <div class="row">
             <div class="col-lg-12">
-                <h1 class="page-header"><?php _e('Oooops... Error 404','sleekr-lite'); ?>
+                <h1 class="page-header"><?php esc_html_e('Oooops... Error 404','sleekr-lite'); ?>
                 </h1>
                 <?php custom_breadcrumbs(); ?>
             </div>
-        </div>
-        <!-- /.row -->
+        </div><!-- /Page Heading/Breadcrumbs -->
 
         <div class="row">
 
-            <div class="col-lg-12">
+            <div class="col-md-12">
                 <div class="jumbotron">
-                    <h1><span class="error-404">404</span>
-                    </h1>
-                    <p><?php _e('The page you\'re looking for could not be found. Here are some helpful links to get you back on track:','sleekr-lite');?></p>
-                    <div class="row">
+                    <h1><span class="error-404">404</span></h1>
+                    <p><?php esc_html_e('The page you\'re looking for could not be found. Here are some helpful links to get you back on track:','sleekr-lite');?></p>
+                    <!-- Custom 404 Widget Areas set from Appearance -> Widgets -->
+		    <div class="row">
                         <?php if ( is_active_sidebar( 's404' ) ) : ?>
                             <div class="col-md-4">
 	                        <?php dynamic_sidebar( 's404' ); ?>
-	                        </div>
+	                    </div>
                         <?php endif; ?>
                         <?php if ( is_active_sidebar( 's404-2' ) ) : ?>
                             <div class="col-md-4">
 	                        <?php dynamic_sidebar( 's404-2' ); ?>
-	                        </div>
+	                    </div>
                         <?php endif; ?>
                         <?php if ( is_active_sidebar( 's404-3' ) ) : ?>
                             <div class="col-md-4">
 	                        <?php dynamic_sidebar( 's404-3' ); ?>
-	                        </div>
+	                    </div>
                         <?php endif; ?>
-                    </div>
-                </div>
-            </div>
+                    </div><!-- .row -->
+                </div><!-- .jumbotron -->
+            </div><!-- .col-md-12 -->
 
-        </div>
+        </div><!-- .row -->
 
 <?php get_footer(); ?>
