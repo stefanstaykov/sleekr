991115706c0c8f3ec25249e8958c51f29bd844d0
whf-chefo <chefopld@gmail.com>
2017-06-20 17:31:35 +0300

    Update archive.php

--- archive.php
+++ archive.php
@@ -56,23 +56,30 @@ get_header(); ?>
 
 <div class="row" style="margin-left:0">
   	<div class="col-md-8 well">
+		<!-- Start the Loop -->
 		<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 			<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+				<!-- Title -->
 	    			<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+				<!-- Date/Time -->
         			<p class="display-time"><i class="fa fa-clock-o"></i> <?php esc_html_e('Posted on ','sleekr-lite'); echo '<a href="'; echo get_day_link(get_post_time('Y'), get_post_time('m'), get_post_time('j')); echo '" class="entry-date">'; the_time('l, F jS, Y'); ?></a></p>
 				<!-- Author Link if isn't an Author Archive Page -->
         			<?php if ( !is_author() ) { echo '<p class="display-author">'; esc_html_e('by ','sleekr-lite'); the_author_posts_link(); echo'</p>'; } ?>
+				<!-- Post Thumbnail -->
         			<?php if ( has_post_thumbnail() ) : ?>
           				<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail( 'sleekr-thumbnail-avatar', array( 'class' => 'img-hover' ) );?></a>
           				<hr>
 				<?php endif ?>
+				<!-- Post Excerpt -->
 	      			<?php the_excerpt() ?>
+				<!-- Read More button -->
 	      			<a class="btn btn-primary" href="<?php the_permalink(); ?>"><?php esc_html_e('Read More','sleekr-lite')?> <i class="fa fa-angle-right"></i></a>
 	      			<hr>
         		</div><!-- /Post -->
 		<?php endwhile; else: ?>
 			<p><?php esc_html_e('Sorry, there are no posts.', 'sleekr-lite'); ?></p>
 		<?php endif; ?>
+		<!-- Custom Pagination -->
 		<?php custom_pagination(); ?>
     	</div><!-- /.col-md-8 .well -->
 	<!-- Sidebar -->
