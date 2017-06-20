9061c4d908ab69f5078930dadde2e3828d3f2e31
whf-chefo <chefopld@gmail.com>
2017-06-20 17:57:27 +0300

    Update comments.php

--- comments.php
+++ comments.php
@@ -1,13 +1,25 @@
 <?php
 /**
- * The template for displaying Comments.
+ * The template for displaying comments
+ *
+ * This is the template that displays the area of the page that contains both the current comments
+ * and the comment form.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Sleekr Lite
  */
 ?>
 		
 <div id="comments">
-	<?php if ( post_password_required() ) : ?>
-		<p class="nopassword"><?php _e( 'This post is password protected. Enter the password to view any comments.', 'sleekr-lite' ); ?></p>
-		</div>
+	<?php /*
+	       * If the current post is protected by a password and
+	       * the visitor has not yet entered the password we will
+	       * return early without loading the comments.
+	       */
+	if ( post_password_required() ) : ?>
+		<p class="nopassword"><?php esc_html_e( 'This post is password protected. Enter the password to view any comments.', 'sleekr-lite' ); ?></p>
+		</div><!-- #comments -->
 		<?php return;
 	endif; ?>
 
@@ -15,9 +27,9 @@
 		<h4 id="comments-title">
 			<?php
 				if( get_comments_number() == 1 ){
-					echo get_comments_number() .' '. __('Comment','sleekr-lite');
+					echo get_comments_number() .' '. esc_html__('Comment','sleekr-lite');
 				} else {
-					echo get_comments_number() .' '. __('Comments','sleekr-lite');
+					echo get_comments_number() .' '. esc_html__('Comments','sleekr-lite');
 				}
 			?>
 		</h4>
@@ -28,44 +40,44 @@
 					'style'       => 'div',
 					'short_ping'  => true,
 					'callback'    => 'sleekr_comments',
-					'reply_text'  => __( 'Reply', 'sleekr-lite' ),
+					'reply_text'  => esc_html__( 'Reply', 'sleekr-lite' ),
 				)); ?>
 		</div>
 
 		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // are there comments to navigate through ?>
 		<nav id="comment-nav" class="pager">
-			<li class="previous"><?php previous_comments_link( __( '&larr; Older Comments', 'sleekr-lite' ) ); ?></li>
-			<li class="next"><?php next_comments_link( __( 'Newer Comments &rarr;', 'sleekr-lite' ) ); ?></li>
+			<li class="previous"><?php previous_comments_link( esc_html__( '&larr; Older Comments', 'sleekr-lite' ) ); ?></li>
+			<li class="next"><?php next_comments_link( esc_html__( 'Newer Comments &rarr;', 'sleekr-lite' ) ); ?></li>
 		</nav>
 		<?php endif; ?>
 
 	<?php
 		elseif ( ! comments_open() && ! is_page() && post_type_supports( get_post_type(), 'comments' ) ) :
 	?>
-		<p class="nocomments"><?php _e( 'Comments are closed.', 'sleekr-lite' ); ?></p>
+		<p class="nocomments"><?php esc_html_e( 'Comments are closed.', 'sleekr-lite' ); ?></p>
 	<?php endif; ?>
 	<?php if ( comments_open() ) : ?>
 	<hr>
 	<!-- Comment Form Custom Code -->
 	<div class="well">
     <?php
-    $required_text = sprintf( ' ' . __('Required fields are marked with %s','sleekr-lite'), '<span class="required">*</span>' );
+    $required_text = sprintf( ' ' . esc_html__('Required fields are marked with %s','sleekr-lite'), '<span class="required">*</span>' );
     $fields =  array(
 
   'author' =>
-    '<p class="comment-form-author">' . __( 'Name ', 'sleekr-lite' ) .
+    '<p class="comment-form-author">' . esc_html__( 'Name ', 'sleekr-lite' ) .
     ( $req ? '<span class="required">*</span>' : '' ) .
     '<input id="author" class="form-control" name="author" type="text" value="' . esc_attr( $commenter['comment_author'] ) .
     '" size="30" /></p>',
 
   'email' =>
-    '<p class="comment-form-email">' . __( 'Email ', 'sleekr-lite' ) .
+    '<p class="comment-form-email">' . esc_html__( 'Email ', 'sleekr-lite' ) .
     ( $req ? '<span class="required">*</span>' : '' ) .
     '<input id="email" class="form-control" name="email" type="text" value="' . esc_attr(  $commenter['comment_author_email'] ) .
     '" size="30" /></p>',
 
   'url' =>
-    '<p class="comment-form-url">' . __( 'Website ', 'sleekr-lite' )  .
+    '<p class="comment-form-url">' . esc_html__( 'Website ', 'sleekr-lite' )  .
     '<input id="url" class="form-control" name="url" type="text" value="' . esc_attr( $commenter['comment_author_url'] ) .
     '" size="30" /></p>',
 );
@@ -73,34 +85,34 @@
 	    'fields'            => $fields,
         'class_form'        => 'form-group',
         'class_submit'      => 'btn btn-primary',
-        'title_reply'       => __( 'Leave a Comment:','sleekr-lite' ),
-        'title_reply_to'    => __( 'Leave a Reply to %s','sleekr-lite' ),
-        'cancel_reply_link' => __( 'Cancel Reply','sleekr-lite' ),
-        'label_submit'      => __( 'Submit','sleekr-lite' ),
+        'title_reply'       => esc_html__( 'Leave a Comment:','sleekr-lite' ),
+        'title_reply_to'    => esc_html__( 'Leave a Reply to %s','sleekr-lite' ),
+        'cancel_reply_link' => esc_html__( 'Cancel Reply','sleekr-lite' ),
+        'label_submit'      => esc_html__( 'Submit','sleekr-lite' ),
 
         'comment_field' =>  '<p class="comment-form-comment"><textarea id="comment" class="form-control" name="comment" rows="3" aria-required="true"></textarea></p>',
 
         'must_log_in' => '<p class="must-log-in">' .
                           sprintf(
-                          __( 'You must be <a href="%s">logged in</a> to post a comment.','sleekr-lite' ),
+                          esc_html__( 'You must be <a href="%s">logged in</a> to post a comment.','sleekr-lite' ),
                           wp_login_url( apply_filters( 'the_permalink', get_permalink() ) )
                           ) . '</p>',
 
         'logged_in_as' => '<p class="logged-in-as">' .
                           sprintf(
-                          __( 'Logged in as <a href="%1$s">%2$s</a>. <a href="%3$s" title="Log out of this account">Log out?</a>','sleekr-lite' ),
+                          esc_html__( 'Logged in as <a href="%1$s">%2$s</a>. <a href="%3$s" title="Log out of this account">Log out?</a>','sleekr-lite' ),
                           admin_url( 'profile.php' ),
                           $user_identity,
                           wp_logout_url( apply_filters( 'the_permalink', get_permalink( ) ) )
                           ) . '</p>',
 
         'comment_notes_before' => '<p class="comment-notes">' .
-                                  __( 'Your email address will not be published.','sleekr-lite' ) . ( $req ? $required_text : '' ) .
+                                  esc_html__( 'Your email address will not be published.','sleekr-lite' ) . ( $req ? $required_text : '' ) .
                                   '</p>',
 
         'comment_notes_after' => '<p class="form-allowed-tags">' .
                                   sprintf(
-                                  __( 'You may use these <abbr title="HyperText Markup Language">HTML</abbr> tags and attributes: %s','sleekr-lite' ),
+                                  esc_html__( 'You may use these <abbr title="HyperText Markup Language">HTML</abbr> tags and attributes: %s','sleekr-lite' ),
                                   ' <code>' . allowed_tags() . '</code>'
                                   ) . '</p>' )
     );
