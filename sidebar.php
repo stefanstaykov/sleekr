8fb15e16a51d098b08bcb3e99d47d34f04200bec
whf-chefo <chefopld@gmail.com>
2017-06-21 20:53:34 +0300

    Update sidebar.php

--- sidebar.php
+++ sidebar.php
@@ -3,7 +3,7 @@
  * The sidebar containing the main widget area
  *
  * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
- *
+ * @since 1.0.0
  * @package Sleekr_Lite
  */
 if ( ! is_active_sidebar( 'main' ) ) {
