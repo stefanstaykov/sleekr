284f0dce65732ae62f3c0ef5fb98a838c5ef8483
whf-chefo <chefopld@gmail.com>
2017-06-21 19:52:40 +0300

    Update searchform.php

--- searchform.php
+++ searchform.php
@@ -1,7 +1,17 @@
+<?php
+/**
+ * The template for the Search Form of the theme.
+ *
+ * @link https://developer.wordpress.org/reference/functions/get_search_form/
+ *
+ * @package Sleekr_Lite
+ */
+?>
+
 <!-- Search from -->
 <form role="search" method="get" action="<?php echo home_url( '/' ); ?>">
     <div class="input-group">
-        <input type="text" class="form-control" placeholder="<?php _e( 'Search...','sleekr-lite' ) ?>" value="" name="s" title="<?php _e( 'Search for:','sleekr-lite') ?>">
+        <input type="text" class="form-control" placeholder="<?php esc_html_e( 'Search...','sleekr-lite' ) ?>" value="" name="s" title="<?php esc_html_e( 'Search for:','sleekr-lite') ?>">
             <span class="input-group-btn">
                 <button class="btn btn-default" type="submit"><i class="fa fa-search"></i></button>
             </span>
