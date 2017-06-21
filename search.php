9d57fcee2e0c39b9e086c9c99f90e865e926ac80
whf-chefo <chefopld@gmail.com>
2017-06-21 20:53:23 +0300

    Update search.php

--- search.php
+++ search.php
@@ -3,7 +3,7 @@
  * The template for displaying search results pages
  *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#search-result
- *
+ * @since 1.0.0
  * @package Sleekr_Lite
  */
 get_header(); ?>
