25e092f6a4c23808b59a4b384f542c0b64a1a9bb
whf-chefo <chefopld@gmail.com>
2017-06-21 19:54:57 +0300

    Update sidebar.php

--- sidebar.php
+++ sidebar.php
@@ -1,3 +1,13 @@
-<?php if ( is_active_sidebar( 'main' ) ) : ?>
-<?php dynamic_sidebar( 'main' ); ?>
-<?php endif; ?>
+<?php
+/**
+ * The sidebar containing the main widget area
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Sleekr_Lite
+ */
+if ( ! is_active_sidebar( 'main' ) ) {
+	return;
+}
+dynamic_sidebar( 'main' ); ?>
+
