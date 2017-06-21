2f3fbcbce7b63edeb1e063491c8e98dbd55e92c1
whf-chefo <chefopld@gmail.com>
2017-06-21 20:55:17 +0300

    Update sleekr-lite.js

--- js/sleekr-lite.js
+++ js/sleekr-lite.js
@@ -2,6 +2,9 @@
  * File sleekr-lite.js.
  * 
  * File for general theme javascript use.
+ *
+ * @since 1.0.0
+ * @package Sleekr_Lite
  */
 
 //Move the static menu bar bellow the WordPress admin bar.
