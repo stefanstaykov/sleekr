ee5bf6cc0e82977f2b5cb419325def96b823d457
whf-chefo <chefopld@gmail.com>
2017-06-29 19:13:54 +0200

    updated language settings

--- footer.php
+++ footer.php
@@ -36,7 +36,7 @@
 		    <!-- Copyright Text -->
                     <div class="col-md-6">
                     	<p><?php esc_html_e('Copyright &copy; ','sleekr-lite'); echo date( 'Y' ).' &middot; '; esc_html_e('All Rights Reserved','sleekr-lite'); echo ' &middot; '. get_bloginfo( 'name' ); ?><br>
-                    	<?php printf ( wp_kses ( __('<a target="_blank" href="%s">Sleekr Lite Theme</a> by','sleekr-lite'), array('a' => array('href' => array(),'target' => array())) ), 'https://www.webhostface.com/wordpress-themes/sleekr-lite/' ); ?> WebHostFace &middot; <?php esc_html_e('Powered by','sleekr-lite'); ?> <a target="_blank" href="https://wordpress.org/">WordPress</a></p>
+                    	<a target="_blank" href="https://www.webhostface.com/wordpress-themes/sleekr-lite/">Sleekr Lite Theme</a> <?php esc_html_e('by','sleekr-lite'); ?> WebHostFace &middot; <?php esc_html_e('Powered by','sleekr-lite'); ?> <a target="_blank" href="https://wordpress.org/">WordPress</a></p>
                     </div><!-- /.col-md-6 Copyright Text -->
 		    <!-- Footer Menu -->
                     <div class="col-md-6">
